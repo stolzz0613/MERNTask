9a8bb8ee999b01cecc2dd5cd4e44aec4ae290455
stolzz0613 <andres0613@utp.edu.co>
2020-07-27 02:33:04 -0500

    Updates

---
+++ precache-manifest.fd9cfd95141a41aea936a21c1aa2d0d1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78ae97732d8659a5fdc20aa36264aa65",
+    "url": "/stolzz0613/MERNTask/index.html"
+  },
+  {
+    "revision": "1276c2e52ccb1cb35ca1",
+    "url": "/stolzz0613/MERNTask/static/css/main.4a4a9a3d.chunk.css"
+  },
+  {
+    "revision": "aba7819cad170bf5e37f",
+    "url": "/stolzz0613/MERNTask/static/js/2.35a59cba.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/stolzz0613/MERNTask/static/js/2.35a59cba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1276c2e52ccb1cb35ca1",
+    "url": "/stolzz0613/MERNTask/static/js/main.fb3a8d2b.chunk.js"
+  },
+  {
+    "revision": "d00041ab05231edea59c",
+    "url": "/stolzz0613/MERNTask/static/js/runtime-main.56eb47a0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/stolzz0613/MERNTask/static/media/logo.5d5d9eef.svg"
+  }
+]);
