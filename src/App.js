0b6b204a535e360c76760a0ca048bd0afe94c501
stolzz0613 <andres0613@utp.edu.co>
2020-07-31 17:45:08 -0500

    Tarea Context-Reducer

--- src/App.js
+++ src/App.js
@@ -4,17 +4,20 @@ import NuevaCuenta from "./components/auth/NuevaCuenta";
 import Proyectos from "./components/proyectos/Proyectos";
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import ProyectoState from "./context/proyectos/ProyectoState";
+import TareaState from "./context/tareas/TareaState";
 
 function App() {
   return (
     <ProyectoState>
-      <Router>
-        <Switch>
-          <Route exact path="/MERNTask" component={Login} />
-          <Route exact path="/MERNTask/nueva-cuenta" component={NuevaCuenta} />
-          <Route exact path="/MERNTask/proyectos" component={Proyectos} />
-        </Switch>
-      </Router>
+      <TareaState>
+        <Router>
+          <Switch>
+            <Route exact path="/MERNTask" component={Login} />
+            <Route exact path="/MERNTask/nueva-cuenta" component={NuevaCuenta} />
+            <Route exact path="/MERNTask/proyectos" component={Proyectos} />
+          </Switch>
+        </Router>
+      </TareaState>
     </ProyectoState>
   );
 }
---
+++ src/context/tareas/TareaState.js
@@ -0,0 +1,19 @@
+import React, { useReducer } from 'react';
+import TareaContext from "./TareaContext";
+import TareaReducer from "./TareaReducer";
+
+const TareaState = props => {
+    const initialState = {
+        tareas: [],
+    }
+
+    const [state, dispatch] = useReducer(TareaReducer, initialState);
+
+    return (
+        <TareaContext.Provider>
+            {props.children}
+        </TareaContext.Provider>
+    )
+}
+
+export default TareaState;
