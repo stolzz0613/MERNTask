444af51994ed0cb665536f33dd9d08e61a9d6344
stolzz0613 <andres0613@utp.edu.co>
2020-08-01 23:28:29 -0500

    tareasstate

--- src/context/tareas/TareaState.js
+++ src/context/tareas/TareaState.js
@@ -6,16 +6,28 @@ const TareaState = props => {
     const initialState = {
         tareas: [
             { nombre: 'Elegir Plataforma', estado: true, proyecto: 1 },
-            { nombre: 'Elegir Colores', estado: false, proyecto: 1 },
+            { nombre: 'Elegir Colores', estado: false, proyecto: 2 },
+            { nombre: 'Elegir Plataformas de Pago', estado: false, proyecto: 3 },
+            { nombre: 'Elegir Hosting', estado: true, proyecto: 4 },
+            { nombre: 'Elegir Plataforma', estado: true, proyecto: 1 },
+            { nombre: 'Elegir Colores', estado: false, proyecto: 2 },
             { nombre: 'Elegir Plataformas de Pago', estado: false, proyecto: 1 },
-            { nombre: 'Elegir Hosting', estado: true, proyecto: 1 },
+            { nombre: 'Elegir Hosting', estado: true, proyecto: 2 },
+            { nombre: 'Elegir Plataforma', estado: true, proyecto: 3 },
+            { nombre: 'Elegir Colores', estado: false, proyecto: 3 },
+            { nombre: 'Elegir Plataformas de Pago', estado: false, proyecto: 1 },
+            { nombre: 'Elegir Hosting', estado: true, proyecto: 3 },
         ],
     }
 
     const [state, dispatch] = useReducer(TareaReducer, initialState);
 
     return (
-        <TareaContext.Provider>
+        <TareaContext.Provider
+            value={{
+                tareas: state.tareas,
+            }}
+        >
             {props.children}
         </TareaContext.Provider>
     )
